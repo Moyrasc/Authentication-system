f5209179d42c3fedca300f79d6a628740bd7e8e1
Moyra_sc <noemisolac@gmail.com>
2022-08-24 09:37:31 +0000

    created endpoint private

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,13 +1,25 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask import Flask, request, jsonify, url_for, Blueprint
+from flask import Flask, request, jsonify, url_for, Blueprint, json
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 
 api = Blueprint('api', __name__)
 
+#Create new user
+# @api.route('signup', methods=['POST'])
+
+# def signup():
+#     body = json.loads(request.data)
+#     new_user = User(email = body ["email"], password = body ["password"])
+#     db.session.add(new_user)
+#     db.session.commit()
+#     response_body = {
+#         "message" : "user ok"
+#     }
+#     return jsonify(new_user.serialize(),200)
 
 @api.route('/login', methods=['POST', 'GET'])
 
@@ -30,3 +42,11 @@ def login():
         "message": "login"
     }
     return jsonify(response_body), 200
+
+#Acces private route
+@api.route('/private',methods =['GET'])    
+@jwt_required()
+def acces_profile():
+    current_user=get_jwt_identity()
+    user = User.query.filter_by(email=current_user).first()
+    return jsonify(user.serialize()), 200
