e56a0c89294d86c5f2ef44ff05b1067763dc8910
Moyra_sc <noemisolac@gmail.com>
2022-08-25 21:37:57 +0000

    change login

--- src/front/js/component/login.js
+++ src/front/js/component/login.js
@@ -1,34 +1,31 @@
 import React from "react";
 
 const Login = () => {
-  return (
-    <form>
-      <div class="mb-3 row">
-        <label for="exampleFormControlInput1" class="col-sm-2 col-form-label">
-          Email
-        </label>
-        <div class="col-sm-10">
-          <input
-            type="email"
-            class="form-control"
-            id="exampleFormControlInput1"
-            value="email@example.com"
-          />
-        </div>
-      </div>
-      <div class="mb-3 row">
-        <label for="inputPassword" class="col-sm-2 col-form-label">
-          Password
-        </label>
-        <div class="col-sm-10">
-          <input type="password" class="form-control" id="inputPassword" />
-        </div>
-      </div>
-      <div class="col-12">
-    <button class="btn btn-primary" type="submit">Login</button>
-  </div>
-    </form>
-  );
+    return (
+        <form className="">
+            <div className="mb-3 row">
+                <label for="exampleFormControlInput1" className=" col-sm-4 col-form-label">
+                    Email
+                </label>
+                <div className="col-md-8">
+                    <input
+                        type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"
+                    />
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="inputPassword" className="col-sm-4 col-form-label">
+                    Password
+                </label>
+                <div className="col-md-8">
+                    <input type="password" className="form-control" id="inputPassword" />
+                </div>
+            </div>
+            <div className="col-12">
+                <button className="btn btn-primary" type="submit">Login</button>
+            </div>
+        </form>
+    );
 };
 
 export default Login;
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -9,8 +9,8 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
+			
+			<p className="d-flex justify-content-center">
 				<Login/>
 			</p>
 			<div className="alert alert-info">
